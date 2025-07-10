7023dfe592c1d5e4240179c496ec7260ca55238a
Kang0408 <trandanhkhang482004@gmail.com>
2025-07-10 15:00:19 +0700

    init back end project

---
+++ Back_end/config/http.js
@@ -0,0 +1,67 @@
+/**
+ * HTTP Server Settings
+ * (sails.config.http)
+ *
+ * Configuration for the underlying HTTP server in Sails.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For more information on configuration, check out:
+ * https://sailsjs.com/config/http
+ */
+const helmet = require("helmet");
+const cors = require("cors");
+
+module.exports.http = {
+  /****************************************************************************
+   *                                                                           *
+   * Sails/Express middleware to run for every HTTP request.                   *
+   * (Only applies to HTTP requests -- not virtual WebSocket requests.)        *
+   *                                                                           *
+   * https://sailsjs.com/documentation/concepts/middleware                     *
+   *                                                                           *
+   ****************************************************************************/
+
+  middleware: {
+    /***************************************************************************
+     *                                                                          *
+     * The order in which middleware should be run for HTTP requests.           *
+     * (This Sails app's routes are handled by the "router" middleware below.)  *
+     *                                                                          *
+     ***************************************************************************/
+    order: [
+      "cookieParser",
+      "session",
+      "bodyParser",
+      "compress",
+      "poweredBy",
+      "router",
+      "www",
+      "favicon",
+      "helmet",
+      "cors",
+    ],
+
+    // Middleware bảo mật Helmet
+    helmet: helmet(),
+
+    // Middleware CORS
+    cors: cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    }),
+    /***************************************************************************
+     *                                                                          *
+     * The body parser that will handle incoming multipart HTTP requests.       *
+     *                                                                          *
+     * https://sailsjs.com/config/http#?customizing-the-body-parser             *
+     *                                                                          *
+     ***************************************************************************/
+    // bodyParser: (function _configureBodyParser(){
+    //   var skipper = require('skipper');
+    //   var middlewareFn = skipper({ strict: true });
+    //   return middlewareFn;
+    // })(),
+  },
+};
